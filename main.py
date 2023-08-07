d51957f476de5c18bd417bafddb4f10cd2c8dc1b
vncntchn05 <vncntchn05@gmail.com>
2023-08-07 15:11:36 -0400

    combine helper into main

--- main.py
+++ main.py
@@ -1,11 +1,50 @@
 import smtplib, ssl
-from helper import spotcheck
+#from helper import spotcheck
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 from urllib3.exceptions import InsecureRequestWarning
 from urllib3 import disable_warnings
 disable_warnings(InsecureRequestWarning)
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+def spotcheck(level, sess, subject, cournum, classnum):
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    data = {
+        'level': level,
+        'sess': sess,
+        'subject': subject,
+        'cournum': cournum,
+    }
+
+    response = requests.post('https://classes.uwaterloo.ca/cgi-bin/cgiwrap/infocour/salook.pl', headers=headers, data=data, verify=False)
+
+    if response != None:
+        html = "" + response.text
+
+        soup = BeautifulSoup(html, 'html.parser')
+
+        datalist = []
+
+        for data in soup.find_all('td'):
+            if data.string is not None:
+                t = re.sub('[^0-9a-zA-Z]+', '', data.string)
+                if t != '':
+                    datalist.append(t)
+
+        wanti = datalist.index(classnum)
+        if datalist[wanti + 7] < datalist[wanti + 6]:
+            return True, "There is an open spot for " + subject + " " + cournum + " (" + classnum + ")"
+        return False, ""
+    return False, ""
+
 
 port = 465
 smtp_server = "smtp.gmail.com"
@@ -48,5 +87,5 @@ if p:
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message.as_string())
-    print("sent email, " + text)
+    #print("sent email, " + text)
 
